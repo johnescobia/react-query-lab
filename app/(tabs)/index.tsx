809427c5ec3dfebe152e1013faa4682b7b823b24
John Escobia <escobiajohn@gmail.com>
2024-05-12 05:36:35 +0800

    feat: load posts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,94 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 import API from '@/services/api';
+import { useQuery } from '@tanstack/react-query';
 
-export default function Screen() {
 
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
-  );
+interface Post {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+export default function Screen(): React.ReactNode {
+  const { data: posts, isLoading, isFetching } = useQuery<Post[], Error>({
+    queryKey: ['posts'],
+    queryFn: async (): Promise<Post[]> => {
+      try {
+        const response = await API.getPost();
+
+        return response.data.map((post: any) => ({
+          ...post,
+          body: post.body.replace(/\n/g, '')  // Strip newlines right after fetching
+        }));
+
+      } catch (error: any) {
+        console.error(error);
+        throw new Error(error);
+      }
+    }
+  });
+
+  // console.log(posts);
+
+  return (
+    <ScrollView
+      style={{
+        flex: 1,
+        backgroundColor: '#fff',
+      }}
+      contentContainerStyle={{
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+      }}
+    >
+      {isLoading || isFetching && <Text>Loading...</Text>}
+      {posts?.map((post: Post) => (
+        <View
+          key={post.id}
+          style={{
+            width: '100%',
+            marginVertical: 10,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 16,
+              fontVariant: ['small-caps'],
+            }}
+          >
+            {post.title}
+          </Text>
+          <Text
+            style={{
+              fontSize: 12,
+            }}
+          >
+            {post.body}
+          </Text>
+        </View>
+      ))
+      }
+      {/* <Text
+        style={{
+          fontSize: 20,
+          fontWeight: 'bold',
+        }}
+      >
+        Home
+      </Text>
+      <View style={{
+        marginVertical: 30,
+        height: 1,
+        width: '80%',
+      }} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" /> */}
+    </ScrollView>
+  );
+}
